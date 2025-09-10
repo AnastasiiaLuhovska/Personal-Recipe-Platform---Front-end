96ee32065a6ca01d38ad63cfb1fc2da38791dcf1
Stacy <stacyxberlin@gmail.com>
2025-09-10 12:04:05 +0200

    fix refresh

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL: "http://localhost:3000",
-    withCredentials: true
+    withCredentials: true,
 
 });
 //    baseURL: "https://personal-recipe-platform-back-end.onrender.com",
